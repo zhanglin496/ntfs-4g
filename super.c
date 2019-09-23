3ecc19259dbae83bf169dee1cb12b95ee6832a3a
zhanglin496 <zhanglin496@163.com>
2019-09-23 10:55:23 +0800

    add ntfs_iget

--- super.c
+++ super.c
@@ -141,6 +141,156 @@ static void ntfs_set_inode(struct inode *inode, dev_t rdev)
 		init_special_inode(inode, inode->i_mode, rdev);
 }
 
+
+static struct dentry *ntfs_lookup(struct inode *dir, struct dentry *dentry,
+				unsigned int flags)
+{
+	ntfs_log_debug("%s\n", __func__);
+	struct inode *inode;
+	return ERR_PTR(-EOPNOTSUPP);
+	ntfs_inode *ni = EXNTFS_I(dir);
+	ni = ntfs_pathname_to_inode(ni->vol, ni, dentry->d_name.name);
+	ntfs_log_debug("ni=%p\n", ni);
+	if (!IS_ERR(ni)) {
+		ntfs_log_debug("error\n");
+		inode = &ni->vfs_inode;
+		inode_init_once(inode);
+		inode_init_always(ni->vol->sb, inode);
+		inode->i_size = ni->data_size;
+		inode->i_ino = ni->mft_no;
+		inode->i_blocks = ni->allocated_size >> 9;
+		inode_set_iversion(inode, 1);
+		INIT_LIST_HEAD(&inode->i_sb_list);
+		if (ni->flags & FILE_ATTR_DIRECTORY)
+			inode->i_mode |= S_IFDIR;
+		else
+			inode->i_mode |= S_IFREG;
+		inode_sb_list_add(inode);
+//		ntfs_inode_close(ni);
+		return d_splice_alias(inode, dentry);
+	}
+
+	return (void *)ni;
+}
+
+static int ntfs_unlink(struct inode *dir, struct dentry *dentry)
+{
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_symlink(struct inode *dir, struct dentry *dentry, const char *target)
+{
+	ntfs_log_debug("%s\n", __func__);
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
+{
+	ntfs_log_debug("%s\n", __func__);
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_rmdir(struct inode *dir, struct dentry *dentry)
+{
+	ntfs_log_debug("%s\n", __func__);
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_rename(struct inode *old_dir, struct dentry *old_dentry,
+			struct inode *new_dir, struct dentry *new_dentry,
+			unsigned int flags)
+{
+	ntfs_log_debug("%s\n", __func__);
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_setattr(struct dentry *dentry, struct iattr *attr)
+{
+	ntfs_log_debug("%s\n", __func__);
+	return -EOPNOTSUPP;
+}
+
+static int ntfs_getattr(const struct path *path, struct kstat *stat,
+			 u32 request_mask, unsigned int flags)
+{
+	ntfs_log_debug("%s\n", __func__);
+	struct inode *inode = path->dentry->d_inode;
+
+	generic_fillattr(inode, stat);
+
+	return 0;
+}
+
+static int ntfs_update_time(struct inode *inode, struct timespec64 *time, int flags)
+{
+	return 0;
+}
+
+const struct inode_operations ntfs_dir_inode_operations = {
+	.create        = __ntfs_create,
+	.lookup        = ntfs_lookup,
+	.unlink        = ntfs_unlink,
+	.symlink       = ntfs_symlink,
+	.mkdir         = ntfs_mkdir,
+	.rmdir         = ntfs_rmdir,
+	.rename        = ntfs_rename,
+	.setattr       = ntfs_setattr,
+	.getattr       = ntfs_getattr,
+	.update_time   = ntfs_update_time,
+};
+
+static long ntfs_generic_ioctl(struct file *filp,
+		unsigned int cmd, unsigned long arg)
+{
+	return -EOPNOTSUPP;
+}
+
+
+static int ntfs_filldir(void *dirent, const ntfschar *name,
+		const int name_len, const int name_type, const s64 pos,
+		const MFT_REF mref, const unsigned dt_type)
+{
+	char data[128];
+	int ret;
+	ret = !dir_emit(dirent, (void *)name, name_len, MREF(mref), dt_type);
+	memcpy(data, name, name_len);
+	data[name_len] = 0;
+	ntfs_log_debug("dir_emit %d, name=%s, len=%d\n", ret, data, name_len);
+	return ret;
+}
+
+static int __ntfs_readdir(struct file *filp, struct dir_context *ctx)
+{
+	struct inode *inode = file_inode(filp);
+	int ret;
+	loff_t cpos;
+	ntfs_log_debug("%s\n", __func__);
+	cpos = ctx->pos;
+
+	ret = ntfs_readdir(EXNTFS_I(inode), &cpos, ctx, ntfs_filldir);
+	if (!ret)
+		ctx->pos = cpos;
+	ntfs_log_debug("%s, ret=%d\n", __func__, ret);
+	return ret;
+}
+
+const struct file_operations ntfs_dir_operations = {
+	.llseek     = generic_file_llseek,
+	.read       = generic_read_dir,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,11,0)
+	.iterate    = __ntfs_readdir,
+#else
+	.readdir    = __ntfs_readdir,
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36)
+	.ioctl      = ntfs_generic_ioctl,
+	.fsync      = ntfs_file_fsync,
+#else
+	.unlocked_ioctl = ntfs_generic_ioctl,
+	.fsync      = generic_file_fsync,
+#endif
+};
+
 static ntfs_inode *ntfs_inode_get(struct super_block *sb,
 				struct inode *inode, const MFT_REF mref)
 {
@@ -305,155 +455,6 @@ error_exit:
 	iget_failed(inode);
 	return ERR_PTR(-EIO);
 }
-
-static struct dentry *ntfs_lookup(struct inode *dir, struct dentry *dentry,
-				unsigned int flags)
-{
-	ntfs_log_debug("%s\n", __func__);
-	struct inode *inode;
-	return ERR_PTR(-EOPNOTSUPP);
-	ntfs_inode *ni = EXNTFS_I(dir);
-	ni = ntfs_pathname_to_inode(ni->vol, ni, dentry->d_name.name);
-	ntfs_log_debug("ni=%p\n", ni);
-	if (!IS_ERR(ni)) {
-		ntfs_log_debug("error\n");
-		inode = &ni->vfs_inode;
-		inode_init_once(inode);
-		inode_init_always(ni->vol->sb, inode);
-		inode->i_size = ni->data_size;
-		inode->i_ino = ni->mft_no;
-		inode->i_blocks = ni->allocated_size >> 9;
-		inode_set_iversion(inode, 1);
-		INIT_LIST_HEAD(&inode->i_sb_list);
-		if (ni->flags & FILE_ATTR_DIRECTORY)
-			inode->i_mode |= S_IFDIR;
-		else
-			inode->i_mode |= S_IFREG;
-		inode_sb_list_add(inode);
-//		ntfs_inode_close(ni);
-		return d_splice_alias(inode, dentry);
-	}
-
-	return (void *)ni;
-}
-
-static int ntfs_unlink(struct inode *dir, struct dentry *dentry)
-{
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_symlink(struct inode *dir, struct dentry *dentry, const char *target)
-{
-	ntfs_log_debug("%s\n", __func__);
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
-{
-	ntfs_log_debug("%s\n", __func__);
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_rmdir(struct inode *dir, struct dentry *dentry)
-{
-	ntfs_log_debug("%s\n", __func__);
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_rename(struct inode *old_dir, struct dentry *old_dentry,
-			struct inode *new_dir, struct dentry *new_dentry,
-			unsigned int flags)
-{
-	ntfs_log_debug("%s\n", __func__);
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_setattr(struct dentry *dentry, struct iattr *attr)
-{
-	ntfs_log_debug("%s\n", __func__);
-	return -EOPNOTSUPP;
-}
-
-static int ntfs_getattr(const struct path *path, struct kstat *stat,
-			 u32 request_mask, unsigned int flags)
-{
-	ntfs_log_debug("%s\n", __func__);
-	struct inode *inode = path->dentry->d_inode;
-
-	generic_fillattr(inode, stat);
-
-	return 0;
-}
-
-static int ntfs_update_time(struct inode *inode, struct timespec64 *time, int flags)
-{
-	return 0;
-}
-
-const struct inode_operations ntfs_dir_inode_operations = {
-	.create        = __ntfs_create,
-	.lookup        = ntfs_lookup,
-	.unlink        = ntfs_unlink,
-	.symlink       = ntfs_symlink,
-	.mkdir         = ntfs_mkdir,
-	.rmdir         = ntfs_rmdir,
-	.rename        = ntfs_rename,
-	.setattr       = ntfs_setattr,
-	.getattr       = ntfs_getattr,
-	.update_time   = ntfs_update_time,
-};
-
-static long ntfs_generic_ioctl(struct file *filp,
-		unsigned int cmd, unsigned long arg)
-{
-	return -EOPNOTSUPP;
-}
-
-
-static int ntfs_filldir(void *dirent, const ntfschar *name,
-		const int name_len, const int name_type, const s64 pos,
-		const MFT_REF mref, const unsigned dt_type)
-{
-	char data[128];
-	int ret;
-	ret = !dir_emit(dirent, (void *)name, name_len, MREF(mref), dt_type);
-	memcpy(data, name, name_len);
-	data[name_len] = 0;
-	ntfs_log_debug("dir_emit %d, name=%s, len=%d\n", ret, data, name_len);
-	return ret;
-}
-
-static int __ntfs_readdir(struct file *filp, struct dir_context *ctx)
-{
-	struct inode *inode = file_inode(filp);
-	int ret;
-	loff_t cpos;
-	ntfs_log_debug("%s\n", __func__);
-	cpos = ctx->pos;
-
-	ret = ntfs_readdir(EXNTFS_I(inode), &cpos, ctx, ntfs_filldir);
-	if (!ret)
-		ctx->pos = cpos;
-	ntfs_log_debug("%s, ret=%d\n", __func__, ret);
-	return ret;
-}
-
-const struct file_operations ntfs_dir_operations = {
-	.llseek     = generic_file_llseek,
-	.read       = generic_read_dir,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,11,0)
-	.iterate    = __ntfs_readdir,
-#else
-	.readdir    = __ntfs_readdir,
-#endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36)
-	.ioctl      = ntfs_generic_ioctl,
-	.fsync      = ntfs_file_fsync,
-#else
-	.unlocked_ioctl = ntfs_generic_ioctl,
-	.fsync      = generic_file_fsync,
-#endif
-};
 
 static void print_hex(void *data, int len)
 {
